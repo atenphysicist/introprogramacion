565ecc244eb8b0d647754dcc0619e44575289d27
atenphysicist <davidjorqcofre@gmail.com>
2025-03-11 14:43:45 -0300

    Add files via upload

---
+++ q pa def.py
@@ -0,0 +1,14 @@
+import os
+print ("Ingresa tus datos")
+vnombreyapellido = input("Nombre y Apellido:")
+vedad = input("Edad:")
+print ("Hola, tu nombre es",vnombreyapellido,"y tienes",vedad,"años")
+print ("Ingresa tu dirección y correo")
+vcorreo = input("Correo:")
+vdireccion = input("Dirección:")
+print ("Tus datos son " ,vcorreo, "y tu dirección es",vdireccion)
+print ("Para concluir facilitanos tu rut!.")
+vrut = input("Rut:")
+print ("Gracias por proporcionar tus datos")
+
+  
